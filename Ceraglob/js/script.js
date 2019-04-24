d91cff570b7c54ebc53d6d660a1d2b46093e9cdf
Prit Bhut <bhutprit786@gmail.com>
2019-04-24 22:26:37 +0530

    Addpages

--- Ceraglob/js/script.js
+++ Ceraglob/js/script.js
@@ -60,14 +60,4 @@ $('.clientsay').owlCarousel({
 
 //Login Page Animation
 
-var signUpButton = document.getElementById("signUp");
-var signInButton = document.getElementById("signIn");
-var container = document.getElementById("container");
 
-signUpButton.addEventListener("click", () =>
-    container.classList.add("right-panel-active")
-    );
-
-signInButton.addEventListener("click", () =>
-    container.classList.remove("right-panel-active")
-    );
